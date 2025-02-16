53ddf060ef5cf80d7c5fc25cfc67f737c85ae2d8
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-02-16 22:32:45 +0200

    Create DB seeder

---
+++ server/utils/seeder.js
@@ -0,0 +1,27 @@
+const { mongooseInit, dispatchMongoose } = require('../config/mongooseInit');
+const Joke = require('../models/Joke');
+
+async function seedDb() { //Probably not the best way to seed DB, but it kinda works
+    await mongooseInit();
+
+    const data = [];
+    const URL = "https://teehee.dev/api/joke";
+    const SEED_COUNT = 1;
+
+    for (let i = 0; i < SEED_COUNT; i++) {
+        const response = await fetch(URL);
+        const { question, answer } = await response.json();
+
+        data.push({
+            question,
+            answer
+        });
+    }
+
+    await Joke.insertMany(data);
+    console.log("Db seeded!");
+
+    dispatchMongoose();
+}
+
+seedDb();
