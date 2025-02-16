589336135ed1c279dd9725bf133fcbb5807d6264
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-02-16 22:31:49 +0200

    Update jokeController

--- server/controllers/jokeController.js
+++ server/controllers/jokeController.js
@@ -1,3 +1,53 @@
-const jokeController = require('express').Router(); 
+const jokeController = require('express').Router();
+const {
+    getRandomJoke,
+    submitVote,
+    updateJoke,
+    deleteJoke
+} = require('../services/jokeService');
+
+jokeController.get('/', async (req, res) => {
+    try {
+        const joke = await getRandomJoke();
+        res.json(joke);
+    } catch (err) {
+        res.status(404).end();
+    }
+});
+
+jokeController.post('/:jokeId', async (req, res) => {
+    const jokeId = req.params.jokeId;
+    const { label } = req.body;
+
+    try {
+        await submitVote(jokeId, label);
+        res.status(201).end();
+    } catch (err) {
+        res.status(400).end();
+    }
+});
+
+jokeController.put('/:jokeId', async (req, res) => {
+    const jokeId = req.params.jokeId;
+    const { question, answer } = req.body;
+
+    try {
+        const joke = await updateJoke(jokeId, question, answer);
+        res.json(joke);
+    } catch (err) {
+        res.status(400).end();
+    }
+});
+
+jokeController.delete('/:jokeId', async (req, res) => {
+    const jokeId = req.params.jokeId;
+
+    try {
+        await deleteJoke(jokeId);
+        res.status(204).end();
+    } catch (err) {
+        res.status(400).end();
+    }
+});
 
 module.exports = jokeController;
