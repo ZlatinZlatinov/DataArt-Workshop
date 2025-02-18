6aa54c2edf92b30114f7b5124fafe699e70cad6a
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-02-18 18:56:00 +0200

    File updates

--- client/voting-game/src/App.tsx
+++ client/voting-game/src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom"
 import Joke from "./components/joke"
+import NextBtn from "./components/nextBtn"
 
 function App() {
 
   return (
-    <section className="flex justify-center items-center w-full h-dvh bg-gray-800">
-      <Joke />
+    <section className="flex flex-col gap-2.5 justify-center items-center w-full h-dvh bg-gray-800">
+      <Routes>
+        <Route path={'/joke'} element={<Joke />} />
+      </Routes> 
+      <NextBtn/>
     </section>
   )
 }
