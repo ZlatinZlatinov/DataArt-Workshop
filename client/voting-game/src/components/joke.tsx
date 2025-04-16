7a94597fda26bfeda21bdc051e7bac7683d815e0
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-04-16 13:16:08 +0300

    Update styling for joke.tsx

--- client/voting-game/src/components/joke.tsx
+++ client/voting-game/src/components/joke.tsx
@@ -6,9 +6,9 @@ export default function Joke({
 }: { joke: JokeType | undefined }) {
 
 
-    return (<div className="p-4 min-w-2xl bg-gray-900 rounded-xl border-1 border-gray-900">
-        <div className="flex justify-center items-start px-4 flex-col gap-1 min-h-28 bg-amber-400 rounded-xl">
-            <p className="text-red-800 text-2xl">Q: {joke?.question || '...'}</p>
+    return (<div className="p-5 w-full min-w-2xl bg-gray-900 rounded-xl shadow-lg border border-gray-700">
+        <div className="flex justify-center items-start p-5 flex-col gap-2 min-h-32 bg-amber-400 rounded-xl shadow-inner">
+            <p className="text-red-800 text-2xl font-medium">Q: {joke?.question || '...'}</p>
             <p className="text-red-800 text-2xl">A: {joke?.answer || '...'}</p>
         </div>
 
