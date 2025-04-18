0f324cf76dc92c13420937e6d7c72a8ff3bc1dd4
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2025-04-18 16:11:03 +0300

    Updated the rest of the components to use JokeContext

--- client/voting-game/src/components/sectionComponent.tsx
+++ client/voting-game/src/components/sectionComponent.tsx
@@ -5,28 +5,31 @@ import { JokeType } from '../types/jokeType';
 import Joke from './Joke/joke';
 import NextBtn from './nextBtn';
 import UpdateComponent from './UpdateJoke/updateComponent';
+import JokeContextProvider from '../contexts/jokeContext';
 
 export default function SectionComponent() {
-    const [joke, setJoke] = useState<JokeType>();
+    // const [joke, setJoke] = useState<JokeType>();
 
-    async function getNextJoke() {
-        const data = await getRandomJoke();
-        setJoke(data);
-    }
+    // async function getNextJoke() {
+    //     const data = await getRandomJoke();
+    //     setJoke(data);
+    // }
 
-    useEffect(() => {
-        try {
-            getNextJoke();
-        } catch (err) {
-            alert(err);
-        }
-    }, []);
+    // useEffect(() => {
+    //     try {
+    //         getNextJoke();
+    //     } catch (err) {
+    //         alert(err);
+    //     }
+    // }, []);
 
-    return (<section className="flex flex-col gap-4 justify-center items-center w-full h-dvh bg-gray-800 px-4">
-        <Routes>
-            <Route path={'/joke'} element={<Joke joke={joke} />} />
-            <Route path={'/update/:jokeId'} element={<UpdateComponent/>} />
-        </Routes>
-        <NextBtn getNextJoke={getNextJoke} />
-    </section>);
+    return (<JokeContextProvider>
+        <section className="flex flex-col gap-4 justify-center items-center w-full h-dvh bg-gray-800 px-4">
+            <Routes>
+                <Route path={'/joke'} element={<Joke />} />
+                <Route path={'/update/:jokeId'} element={<UpdateComponent />} />
+            </Routes>
+            <NextBtn />
+        </section>
+    </JokeContextProvider>);
 }
